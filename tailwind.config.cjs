e5e3fe51d325c1d6979b30459e68979e342d084f
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-01-24 20:48:49 +0100

    feat Add tailwind configurations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        sans: ["poppins", "sans-serif"],
+      },
+      colors: {
+        site: {
+          primary: "#58d777",
+          dark: {
+            400: "#474d84",
+            DEFAULT: "#2f3462",
+            600: "#17193b",
+            700: "#11132e",
+          },
+          light: {
+            400: "#ffffff",
+            DEFAULT: "#e4e6eb",
+            600: "#eef1fb",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
