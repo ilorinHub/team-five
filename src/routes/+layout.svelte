6b6ab9a0e5a32267871e28c845c72b54452c89bc
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-01-26 05:11:32 +0100

    feat add data source selection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,13 @@
   function toggleIsMenuOpen() {
     isMenuOpen = !isMenuOpen;
   }
+
+  let resultSource = "inec";
+  let isResultSelectorOpen = false;
+
+  function toggleIsResultSelectorOpen() {
+    isResultSelectorOpen = !isResultSelectorOpen;
+  }
 </script>
 
 <div class="site_container" class:dark={isDarkMode}>
@@ -27,7 +34,27 @@
           <h2 class="text-lg ">elect<span class="font-medium">Viz</span></h2>
         </a>
         <div class="flex items-center gap-4">
-          <button class="bg-slate-900">Data source</button>
+          <button
+            class="p-2 relative px-4 py-2 bg-site-dark-600 group"
+            on:click={toggleIsResultSelectorOpen}
+          >
+            <span class="text-site-light">
+              {resultSource}
+              <span
+                class="w-6 h-6 bg-green-500 rounded-full inline-flex gap-2 justify-center items-center"
+              >
+                &check;
+              </span>
+            </span>
+            <div
+              class="absolute mt-2 right-0 shadow-md p-1 bg-site-light-600 text-site-dark"
+              class:hidden={!isResultSelectorOpen}
+            >
+              <p class="px-4 py-2">asa</p>
+              <p class="px-4 py-2">asaasdf</p>
+              <p class="px-4 py-2">asaasdfa</p>
+            </div>
+          </button>
           <button on:click={toggleIsMenuOpen}>Menu <span>&#9776;</span></button>
           <div
             class={`fixed top-0 right-0 w-screen h-screen z-10 bg-default backdrop-blur-sm bg-opacity-20 ${
@@ -35,7 +62,7 @@
             } transition-default`}
           >
             <div
-              class="max-w-6xl container py-6 h-full relative fixed inset-0 flex items-center justify-center "
+              class="max-w-6xl container py-6 h-full relative inset-0 flex items-center justify-center "
             >
               <button
                 on:click={toggleIsMenuOpen}
